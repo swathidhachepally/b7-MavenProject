56c163503a52940bdcb00d710de138fd82206548
cubeiplKumar <34553586+cubeiplKumar@users.noreply.github.com>
2023-11-10 20:08:55 +0530

    Update App.java

--- src/main/java/VirtualTechies/App.java
+++ src/main/java/VirtualTechies/App.java
@@ -10,4 +10,16 @@ public class App
     {
         System.out.println( "SampleMaven Project Created !!!!" );
     }
+ public static void main(String[] args) {
+    
+    int num1 = 25;
+    int num2 = 15;
+
+    // create an object of Main
+    Main obj = new Main();
+    // calling method
+    int result = obj.addNumbers(num1, num2);
+    System.out.println("Sum is: " + result);
+  }
+}
 }
