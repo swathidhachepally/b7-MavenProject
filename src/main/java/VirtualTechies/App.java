4740616c3311cedaa0c5e666aba03423db78ce7f
swathidhachepally <40208563+swathidhachepally@users.noreply.github.com>
2024-01-12 10:55:17 -0600

    Update App.java

--- src/main/java/VirtualTechies/App.java
+++ src/main/java/VirtualTechies/App.java
@@ -9,17 +9,25 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "SampleMaven Project Created !!!!" );
-    }
- public static void main(String[] args) {
-    
     int num1 = 25;
     int num2 = 15;
 
     // create an object of Main
-    Main obj = new Main();
+   // App obj = new App();
     // calling method
-    int result = obj.addNumbers(num1, num2);
+    int result = num1 + num2;
     System.out.println("Sum is: " + result);
-  }
+    }
+//  public static void main(String[] args) {
+    
+//     int num1 = 25;
+//     int num2 = 15;
+
+//     // create an object of Main
+//     Main obj = new Main();
+//     // calling method
+//     int result = obj.addNumbers(num1, num2);
+//     System.out.println("Sum is: " + result);
+//   }
 }
-}
+// }
